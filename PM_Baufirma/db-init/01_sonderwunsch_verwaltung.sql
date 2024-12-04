1b0c4ce685c8a965e9da0c422510855c77c22309
ArchdragonPeak <archdragonpeak@gmail.com>
2024-12-04 19:02:19 +0100

    improve database, added Häuse and Sonderwünsche

--- PM_Baufirma/db-init/01_sonderwunsch_verwaltung.sql
+++ PM_Baufirma/db-init/01_sonderwunsch_verwaltung.sql
@@ -3,22 +3,14 @@ USE SonderwunschVerwaltung;
 
 CREATE TABLE Haus (
     Hausnummer INT NOT NULL PRIMARY KEY, -- 1 bis 24
-    Typ VARCHAR(50) NOT NULL -- vielleicht anderer Typ?
-);
-
--- vielleicht?
-CREATE TABLE Kategorie (
-    KategorieID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL
+    HatDachgeschoss BOOLEAN NOT NULL
 );
 
 CREATE TABLE Sonderwunsch (
     SonderwunschID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Beschreibung TEXT NOT NULL,
-    Preis DECIMAL(10, 2) NOT NULL, -- Preis unter 100 Millionen
-    AnzahlVerfuegbar INT,
-    KategorieID INT NOT NULL,
-    FOREIGN KEY (KategorieID) REFERENCES Kategorie(KategorieID) -- siehe Kategorie
+    Preis DECIMAL(10, 2), -- Unter 100 Millionen
+    AnzahlVerfuegbar INT DEFAULT NULL
 );
 
 CREATE TABLE Kunde (
@@ -28,14 +20,14 @@ CREATE TABLE Kunde (
     Email VARCHAR(255) NOT NULL,
     Telefonnummer VARCHAR(20) NOT NULL,
     Hausnummer INT NOT NULL,
-    FOREIGN KEY (Hausnummer) REFERENCES Haus(Hausnummer)
+    FOREIGN KEY (Hausnummer) REFERENCES Haus(Hausnummer) ON DELETE CASCADE
 );
 
 CREATE TABLE Kundenwunsch (
-    KundenwunschID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Kundennummer INT NOT NULL,
     SonderwunschID INT NOT NULL,
-    Anzahl INT NOT NULL,
-    FOREIGN KEY (Kundennummer) REFERENCES Kunde(Kundennummer),
-    FOREIGN KEY (SonderwunschID) REFERENCES Sonderwunsch(SonderwunschID)
+    Anzahl INT NOT NULL CHECK (Anzahl > 0),
+    PRIMARY KEY (Kundennummer, SonderwunschID), -- Zusammengesetzter Primärschlüssel
+    FOREIGN KEY (Kundennummer) REFERENCES Kunde(Kundennummer) ON DELETE CASCADE,
+    FOREIGN KEY (SonderwunschID) REFERENCES Sonderwunsch(SonderwunschID) ON DELETE CASCADE
 );
