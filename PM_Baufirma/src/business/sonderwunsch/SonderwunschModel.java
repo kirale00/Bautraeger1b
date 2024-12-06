619a4c6334f36fd18ec9dd149a16b62f0de146a3
MalteSchmidtOp <malte.schmidt@stud.hs-bochum.de>
2024-12-06 01:53:50 +0100

    KategorieId angepasst beim Konstruktor initiieren

--- PM_Baufirma/src/business/sonderwunsch/SonderwunschModel.java
+++ PM_Baufirma/src/business/sonderwunsch/SonderwunschModel.java
@@ -35,12 +35,13 @@ public class SonderwunschModel {
 	
 	private ArrayList<Sonderwunsch> fetchSonderwuensche() { // durch datenbankzugriff ersetzen, sobald DB fertig
 		ArrayList<Sonderwunsch> swArr = new ArrayList<Sonderwunsch>();
-		swArr.add(new Sonderwunsch("Wand zur Abtrennung der Küche von dem Essbereich", 300,1));
-		swArr.add(new Sonderwunsch("Tür in der Wand zwischen Küche und Essbereich", 300,2));
-		swArr.add(new Sonderwunsch("Großes Zimmer im OG statt zwei kleinen Zimmern", 0,3));
-		swArr.add(new Sonderwunsch("Abgetrennter Treppenraum im DG", 890,4));
-		swArr.add(new Sonderwunsch("Vorrichtung eines Bades im DG", 990,5));
-		swArr.add(new Sonderwunsch("Ausführung eines Bades im DG", 8990,6));
+		final int KATEGORIEID=1;
+		swArr.add(new Sonderwunsch("Wand zur Abtrennung der Küche von dem Essbereich", 300,KATEGORIEID));
+		swArr.add(new Sonderwunsch("Tür in der Wand zwischen Küche und Essbereich", 300,KATEGORIEID));
+		swArr.add(new Sonderwunsch("Großes Zimmer im OG statt zwei kleinen Zimmern", 0,KATEGORIEID));
+		swArr.add(new Sonderwunsch("Abgetrennter Treppenraum im DG", 890,KATEGORIEID));
+		swArr.add(new Sonderwunsch("Vorrichtung eines Bades im DG", 990,KATEGORIEID));
+		swArr.add(new Sonderwunsch("Ausführung eines Bades im DG", 8990,KATEGORIEID));
 		
 
 		return swArr;
